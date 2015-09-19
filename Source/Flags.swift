5cf554ab7151fe3e761512dc44292986eca1f4f6
Max Adamski <max@supercoding.net>
2015-09-20 00:50:37 +0200

    Added Regex struct
    + Regex.Flags option type

---
+++ Source/Flags.swift
@@ -0,0 +1,27 @@
+
+import Darwin
+
+public extension Regex
+{
+    public struct Flags: OptionSetType
+    {
+        public let rawValue: Int32
+        
+        public init(rawValue: Int32)
+        {
+            self.rawValue = rawValue
+        }
+        
+        /// Do not differentiate case.
+        static let IgnoreCase = Flags(rawValue: REG_ICASE)
+        
+        /// Use POSIX Extended Regular Expression syntax.
+        static let Extended = Flags(rawValue: REG_EXTENDED)
+        
+        /// Treat a newline in string as dividing string into multiple lines, so that `$` can match before the newline and `^` can match after. Also, don't permit `.` to match a newline, and don't permit `[^...]` to match a newline.
+        static let Newline = Flags(rawValue: REG_NEWLINE)
+        
+        /// Do not report position of matches.
+        static let NoRanges = Flags(rawValue: REG_NOSUB)
+    }
+}
---
+++ Source/Regex.swift
@@ -0,0 +1,7 @@
+
+import Darwin
+
+public struct Regex
+{
+    
+}
