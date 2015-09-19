38945f5623213c2fc15b825af19b2ae2986c0957
Max Adamski <max@supercoding.net>
2015-09-20 00:54:50 +0200

    Added initializer
    + Turned on code coverage collection
    + Tests

--- Source/Regex.swift
+++ Source/Regex.swift
@@ -3,5 +3,12 @@ import Darwin
 
 public struct Regex
 {
+    public var expression: String
+    public var flags: Flags
     
+    public init(_ expression: String, flags: Flags = [.Extended])
+    {
+        self.expression = expression
+        self.flags = flags
+    }
 }
--- Tests/RegexTests.swift
+++ Tests/RegexTests.swift
@@ -5,5 +5,10 @@ import XCTest
 
 class RegexTests: XCTestCase
 {
-
+    func testItCreates()
+    {
+        let regex = Regex("hello")
+        expect(regex.expression) == "hello"
+        expect(regex.flags) == [.Extended]
+    }
 }
